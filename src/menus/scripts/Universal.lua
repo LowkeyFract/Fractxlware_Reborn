ea35bea6709d490b0ef0116745a644ae2fa03b5a
LowkeyFract <LowkeyFract@gmail.com>
2025-08-24 02:15:38 +0100

    Adding debugs to see why universal isnt working

--- src/menus/scripts/Universal.lua
+++ src/menus/scripts/Universal.lua
@@ -1,7 +1,12 @@
 return function(win, WindUI)
-            WindUI:Notify({
-                Title = "Universal",
-                Content = "This is the universal script!",
-                Icon = "check"
-            })
+    print("Window object:", win)
+    print("Has Section method:", win.Section ~= nil)
+
+    local section = win:Section({
+        Title = "Universal Features",
+        Icon = "star",
+        Opened = true
+    })
+
+    print("Section created:", section)
 end
